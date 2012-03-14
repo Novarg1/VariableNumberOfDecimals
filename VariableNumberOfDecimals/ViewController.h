226f29d8b0a706891f77c041875ac1b7afce7438
admin <admin@Mac-admin.local>
2012-03-14 10:43:16 +0200

    Initial Commit

---
+++ VariableNumberOfDecimals/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VariableNumberOfDecimals
+//
+//  Created by admin on 14/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
